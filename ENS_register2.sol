eb5fdbfd611f1e9ceef6f8b24286b40b19860b7b
sabaka-pes <monsutaaisu@gmail.com>
2024-10-06 14:48:29 +0300

    Update ENS_register2.sol
    Revised

--- ENS_register2.sol
+++ ENS_register2.sol
@@ -12,7 +12,7 @@ contract ENSRegister2 {
     address public immutable owner;
     modifier onlyOwner() {
         require(msg.sender == owner, "You are not an owner!");
-        _; // выполнить тело той функции, к которой был применён этот модификатор
+        _; 
     }
     constructor() {
         owner = msg.sender;
@@ -24,54 +24,54 @@ contract ENSRegister2 {
 
     uint public yearPrice;
     function setYearPrice(uint _newPrice) public onlyOwner {
-        yearPrice = _newPrice; // установка новой цены за год
+        yearPrice = _newPrice; // setting a new price for the year (in wei)
     }
 
     uint public coeff;
     function setCoeff(uint _newCoeff) public onlyOwner {
-        coeff = _newCoeff; // установка нового коэффициента
+        coeff = _newCoeff; // setting a new coefficient (in wei)
     }
 
-    // функция для регистрации домена
+    // Function for domain registration
     function registerENS(string memory _name, uint _yearsRent) public payable {
         uint totalTime = _yearsRent * 365 * 24 * 60 * 60;
 
-        // Проверка: домен должен быть свободен для регистрации
-        require((ensOwner[_name].oAddr == address(0)) && (block.timestamp >= ensOwner[_name].timeOfCreation + totalTime), "Domain is already registered.");
+        // Check: the domain must be available for registration
+        require((ensOwner[_name].oAddr == address(0)) || (block.timestamp <= ensOwner[_name].timeOfCreation + totalTime), "Domain is already registered.");
 
-        // Проверка на валидность временного промежутка для регистрации домена
+        // Checking the validity of the time period for domain registration
         require(_yearsRent >= 1 && _yearsRent <= 10, "Invalid year registration.");
 
+        // Check: the user has paid enough for registration
+        require(msg.value >= (yearPrice * _yearsRent), "Not enough ether provided to register domain.");
 
-        // Проверка: пользователь заплатил достаточно за регистрацию
-        require(msg.value >= (yearPrice * _yearsRent) * 1 ether, "Not enough ether provided to register domain.");
-
-        ensOwner[_name].oAddr = msg.sender; // присвоение доменного имени
-        ensOwner[_name].timeOfCreation = block.timestamp; // время присвоения
-        ensOwner[_name].price = yearPrice; // цена, которую заплатили за домен
-        ensOwner[_name].yearsRent = _yearsRent; // на сколько лет регистрация домена
+        ensOwner[_name].oAddr = msg.sender; // domain name assignment
+        ensOwner[_name].timeOfCreation = block.timestamp; // time of assignment
+        ensOwner[_name].price = yearPrice; // price paid for the domain (per year)
+        ensOwner[_name].yearsRent = _yearsRent; // how many years is the domain registration for
     }
 
-    function extendRegistration(string memory _name, uint _yearsRent) public payable {
-        // Проверка: принадлежит ли домен отправителю транзакции на данный момент
+    // Function for domain renewal
+    function renewRegistration(string memory _name, uint _yearsRent) public payable {
+        // Check: Does the domain currently belong to the sender of the transaction
         require(ensOwner[_name].oAddr == msg.sender, "You are not an owner of this domain or it is not yet expired");
 
-        uint extendPrice = ensOwner[_name].price * coeff;
-        // Проверка: достаточно эфира для продления 
-        require(msg.value >= (extendPrice * _yearsRent) * 1 ether, "Not enough ether provided to extend domain registration.");
+        uint renewPrice = ensOwner[_name].price * coeff;
+        // Check: Are there enough funds for renewal
+        require(msg.value >= (renewPrice * _yearsRent), "Not enough ether provided to extend domain registration.");
 
-        ensOwner[_name].yearsRent += _yearsRent; // на сколько всего лет регистрация домена
+        ensOwner[_name].yearsRent += _yearsRent; // how many years is the domain registration for
     }
 
     function getOwner(string memory _name) public view returns(address, uint) {
-        return (ensOwner[_name].oAddr, ensOwner[_name].yearsRent); // возвращаем владельца домена и срок
+        return (ensOwner[_name].oAddr, ensOwner[_name].yearsRent); // return the domain owner and the term
     }
 	
-	// функция для вывода всех полученных денег
+	// Function to withdraw all received money
     function withdrawAll() public onlyOwner {   
         uint balanceToSend = address(this).balance; 
-        totalWithdrawn += balanceToSend; // сумма выведенных средств
-        payable(owner).transfer(balanceToSend); // вывод всех имеющихся средств
+        totalWithdrawn += balanceToSend; // amount of funds withdrawn
+        payable(owner).transfer(balanceToSend); // withdrawal of all available funds
     }
 
     // function destroy() public {
